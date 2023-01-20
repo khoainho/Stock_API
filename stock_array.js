b7235422678f8941fd547ee7fbd9e1be3fe79f99
Khoa Nguyen <60150482+khoainho@users.noreply.github.com>
2023-01-19 22:38:16 -0600

    Added stocks

---
+++ stock_array.js
@@ -0,0 +1,154 @@
+const stocks = [
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+    {
+        ticker: 'AAPL',
+        companyName: 'Apple Inc',
+        currPrice: 345.34,
+        volume: 23423874
+    },
+]
+
+module.exports = stocks;
